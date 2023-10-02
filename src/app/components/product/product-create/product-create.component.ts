a09b60877738f5f4067ae85492de76de965f13f4
Allan <allan_s11@hotmail.com>
2023-10-02 10:25:45 -0300

    Configurando o botao de editar.

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -13,8 +13,8 @@ import { Product } from '../product.model';
 export class ProductCreateComponent implements OnInit {
 
   product: Product = {
-    name: 'Produto de teste',
-    price: 25.00
+    name: '',
+    price: 0,
   }
   constructor(private productService: ProductService,
       private router: Router){}
